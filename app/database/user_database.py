7ea2109dc901faa41ebb5e157058fc6e22ee6746
Ivan Kozhin <kothin.ivan@gmail.com>
2023-06-06 19:13:06 +0500

    Update user_database.py
    In this version my program is looking for already existing table in the database

--- app/database/user_database.py
+++ app/database/user_database.py
@@ -1,8 +1,10 @@
+import os
+
+from sqlalchemy import inspect
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.asyncio import create_async_engine  # type: ignore
 from sqlalchemy.ext.declarative import declarative_base  # type: ignore
 from sqlalchemy.orm import sessionmaker  # type: ignore
-from sqlalchemy.ext.asyncio import AsyncSession
-import os
 
 host = os.getenv('DB_HOST', 'localhost')
 port = os.getenv('DB_PORT', '5432')
@@ -13,13 +15,23 @@ dbtype = os.getenv('DB_TYPE', 'postgresql')
 
 SQLALCHEMY_DATABASE_URL = f"{dbtype}+asyncpg://{user}:{password}@{host}:{port}/{db}"
 
-
 engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 Base = declarative_base()
 async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
-async def init_models():
+def get_table_names(sync_conn):
+    inspector = inspect(sync_conn)
+    return inspector.get_table_names()
+
+
+async def get_table_names_async():
     async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
-        await conn.run_sync(Base.metadata.create_all)
+        return await conn.run_sync(get_table_names)
+
+
+async def init_models():
+    table_names = await get_table_names_async()
+    if db not in table_names:
+        async with engine.begin() as conn:
+            await conn.run_sync(Base.metadata.create_all)

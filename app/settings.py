0b99c0c5579474fc95780dcab8ee36726a331f16
Ivan Kozhin <kothin.ivan@gmail.com>
2023-06-09 20:55:07 +0500

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -1,4 +1,4 @@
-import socket
+import os
 
 HOST = '0.0.0.0'
 PORT = 8000
@@ -8,19 +8,4 @@ DB_USER = 'user'
 DB_PASSWORD = '123456789'
 DB_NAME = 'postgresdb'
 DB_TYPE = 'postgresql'
-
-
-# Определение внешнего IP-адреса сервера
-def get_server_ip():
-    try:
-        # Создаем сокет для получения внешнего IP-адреса
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.connect(("8.8.8.8", 80))  # Подключаемся к внешнему серверу (в данном случае используем Google DNS)
-        server_ip = sock.getsockname()[0]  # Получаем IP-адрес сервера
-        sock.close()
-        return server_ip
-    except socket.error:
-        return "0.0.0.0"  # В случае ошибки возвращаем 0.0.0.0
-
-
-HOST_OUT = get_server_ip()
+HOST_OUT = os.environ.get("LOCAL_IP")

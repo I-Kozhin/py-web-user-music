c8c0c522e88ed5e87aeea62dfd8a57ed9d8bc909
Ivan Kozhin <kothin.ivan@gmail.com>
2023-06-11 22:18:25 +0500

    Created HTTPException in user_service

--- app/services/user.py
+++ app/services/user.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session  # type: ignore
 
 from app.models.user import UserDto
 from app.repositories.user import UserRepository
+from app.errors import logger
 
 
 class UserService:
@@ -13,12 +14,18 @@ class UserService:
         self.user_repository = UserRepository()
 
     async def create_user(self, user_name: str, session: AsyncSession) -> UserDto:
-        if self.is_valid_name(user_name):
-            user = await self.user_repository.add_user_to_database(user_name, session)
-            return UserDto.create_from_bd(user)
-        else:
-            raise HTTPException(status_code=400, detail=f'Username is incorrect.')
+        try:
+            if self.is_valid_name(user_name):
+                user = await self.user_repository.add_user_to_database(user_name, session)
+                return UserDto.create_from_bd(user)
+        except Exception as e:
+            logger.exception(f'Failed to perform {self.create_user} func at {UserService}: {e}')
+            raise HTTPException(status_code=500,
+                                detail=f'An unexpected error occurred while creating user.')
 
     @staticmethod
     def is_valid_name(user_name: str) -> bool:
-        return (len(user_name)) > 0 and not (user_name.isspace())
+        if (len(user_name)) > 0 and not (user_name.isspace()):
+            return True
+        else:
+            raise HTTPException(status_code=400, detail=f'Username is incorrect.')

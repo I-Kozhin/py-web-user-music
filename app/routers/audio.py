4eb531b4cee3a5cc9e042648cb1d0bf8a3baa653
Ivan Kozhin <kothin.ivan@gmail.com>
2023-06-11 22:22:07 +0500

    Created HTTPException in audio_router

--- app/routers/audio.py
+++ app/routers/audio.py
@@ -1,10 +1,11 @@
-from fastapi import APIRouter, Depends, Response, File, UploadFile, Request
+from fastapi import APIRouter, Depends, Response, File, UploadFile, Request, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from urllib.parse import urlparse
 
 from app.database.database_session_manager import get_session
 from app.services.audio import AudioService
 from app.settings import PORT
+from app.errors import logger
 
 audio_router = APIRouter()
 audio_service = AudioService()
@@ -17,17 +18,20 @@ async def add_audio(user_id: int, user_token: str, request: Request, audio_wav: 
         audio = await audio_service.create_audio(user_id, user_token, audio_wav, session)
         parsed_url = urlparse(f'{request.url}')
         ip_address = parsed_url.hostname
-    except:
-        raise
-    return f"http://{ip_address}:{PORT}/record?audio_id={audio.audio_id}&user_id={audio.user_id}"
+        return f"http://{ip_address}:{PORT}/record?audio_id={audio.audio_id}&user_id={audio.user_id}"
+    except Exception as e:
+        logger.exception(f'Failed to perform {add_audio} func: {e}')
+        raise HTTPException(status_code=500,
+                            detail=f'An unexpected error occurred while doing post-request.')
 
 
 @audio_router.get("/record", response_model=bytes, name="get_audio")
 async def get_audio(audio_id: str, user_id: int, session: AsyncSession = Depends(get_session)) -> Response:
     try:
         audio = await audio_service.get_audio(audio_id, user_id, session)
-    except:
-        raise
-
-    return Response(content=audio.audio_data, media_type='audio/mpeg',
-                    headers={'Content-Disposition': 'attachment; filename=audio_you_wanted.mp3'})
+        return Response(content=audio.audio_data, media_type='audio/mpeg',
+                        headers={'Content-Disposition': 'attachment; filename=audio_you_wanted.mp3'})
+    except Exception as e:
+        logger.exception(f'Failed to perform {get_audio} func: {e}')
+        raise HTTPException(status_code=500,
+                            detail=f'An unexpected error occurred while doing get-request.')

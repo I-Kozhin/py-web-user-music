cf90d5b4b4969cadd5acd895612609db6c8dc8f4
Ivan Kozhin <kothin.ivan@gmail.com>
2023-06-11 21:10:28 +0500

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -5,18 +5,6 @@ from app.database.database import Base
 from sqlalchemy.orm import relationship
 
 
-class UserDto(BaseModel):
-    user_id: Optional[int]
-    user_token: Optional[str]
-
-    @classmethod
-    def create_from_bd(cls, db_row: 'User') -> 'UserDto':
-        return cls(
-            user_id=db_row.user_id,
-            user_token=db_row.user_token
-        )
-
-
 class User(Base):
     """
     Defines the user`s model
@@ -29,3 +17,15 @@ class User(Base):
     user_token = Column(String, unique=True)
 
     audios = relationship("Audio", back_populates="user")
+
+
+class UserDto(BaseModel):
+    user_id: Optional[int]
+    user_token: Optional[str]
+
+    @classmethod
+    def create_from_bd(cls, db_row: User) -> 'UserDto':
+        return cls(
+            user_id=db_row.user_id,
+            user_token=db_row.user_token
+        )
